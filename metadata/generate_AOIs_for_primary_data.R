50dc1a569604dce919c6ab5a6ea23ecb22c1dbaa
Adrian Steffan <adrian.steffan@hotmail.de>
2022-12-16 20:04:30 +0100

    WIP helper functions

--- metadata/generate_AOIs_for_primary_data.R
+++ metadata/generate_AOIs_for_primary_data.R
@@ -13,60 +13,6 @@
 ## 1920 x 1080 screen and 1200 x 900 video
 ## 1920 x 1200 screen and 1200 x 900 video
 
-# generate appropriate specification:
-# aoi_regions
-# aoi_region_id, l_x_max, l_x_min, l_y_max, l_y_min, r_x_max, r_x_min, r_y_max, w_x_max, w_x_min, w_y_max, w_y_min
-generate_aoi_regions <- function(screen_width = 1280, 
-                                 screen_height = 1024,
-                                 video_width = 1280,
-                                 video_height = 960) {
-  
-  ## the approach to genrate the AOIs is as follows:
-  #1. take the tunnel exit diameter of D units. 
-  
-  #2. create a rectangular bounding box that extends D units from 
-  # the uppermost, leftmost, rightmost, and bottommost points of the tunnel exit
-  
-  # we do this calculation for the 1200 x 900 video below, 
-  # and express the result in terms of ratios of video heights and widths. 
-  # since the x/y ratios are the same for the two video sizes, i.e. (1280 x 960) / (1200 x 900) = 1
-  # the AOIs will scale correctly to the 1280 x 960 case. This is done in the function below
-  ratios = ratios_of_bounding_box()
-  
-  aoi_regions = tibble(
-    aoi_region_id = 0, 
-    l_x_max = (video_width*ratios$L_right) + (screen_width-video_width)/2,
-    l_x_min = (video_width*ratios$L_left) + (screen_width-video_width)/2,
-    l_y_max = (video_height*ratios$bottom) + ((screen_height-video_height)/2),
-    l_y_min = (video_height*ratios$top) + ((screen_height-video_height)/2),
-    r_x_max = (video_width*ratios$R_right)  + (screen_width-video_width)/2,
-    r_x_min = (video_width*ratios$R_left)  + (screen_width-video_width)/2,
-    r_y_max = (video_height*ratios$bottom) + ((screen_height-video_height)/2),
-    r_y_min =  (video_height*ratios$top) + ((screen_height-video_height)/2),
-    w_x_max = video_width/2 + floor((video_width*.94)/16) + (screen_width-video_width)/2,
-    w_x_min = video_width/2 - floor((video_width*.94)/16) + (screen_width-video_width)/2,
-    w_y_max = video_height/2 + ((screen_height-video_height)/2),
-    w_y_min = video_height/2 - (video_height*2.5)/16 + ((screen_height-video_height)/2),
-    lb_x_max = (video_width*.23) + (screen_width-video_width)/2,
-    lb_x_min = (video_width*.03) + (screen_width-video_width)/2,
-    lb_y_max = (video_height*0.92) + ((screen_height-video_height)/2),
-    lb_y_min = (video_height*0.62) + ((screen_height-video_height)/2),
-    rb_x_max = video_width - (video_width*.23) + (screen_width-video_width)/2,
-    rb_x_min = video_width - (video_width*.03)  + (screen_width-video_width)/2,
-    rb_y_max = (video_height*0.92) + ((screen_height-video_height)/2),
-    rb_y_min =  (video_height*0.62) + ((screen_height-video_height)/2),
-    lbig_x_max = (video_width*7)/16 + (screen_width-video_width)/2,
-    lbig_x_min = (screen_width-video_width)/2,
-    lbig_y_max = video_height + ((screen_height-video_height)/2),
-    lbig_y_min = ((video_height*9)/16) + ((screen_height-video_height)/2),
-    rbig_x_max = video_width + (screen_width-video_width)/2,
-    rbig_x_min = ((video_width*9)/16) + (screen_width-video_width)/2,
-    rbig_y_max = video_height + ((screen_height-video_height)/2),
-    rbig_y_min = ((video_height*9)/16) + ((screen_height-video_height)/2)
-  )
-}
-
-
 ratios_of_bounding_box <- function() {
   
   # compute ratios using these video dimensions
@@ -104,6 +50,84 @@ ratios_of_bounding_box <- function() {
               bottom = bottom_ratio))
 }
 
+ratios <-ratios_of_bounding_box()
+video_width <- 1280
+video_height <- 960
+
+aoi_region_sets = tibble(
+  aoi_region_set_id = 0, 
+  l_x_max = (video_width*ratios$L_right),
+  l_x_min = (video_width*ratios$L_left),
+  l_y_max = (video_height*ratios$bottom),
+  l_y_min = (video_height*ratios$top),
+  r_x_max = (video_width*ratios$R_right),
+  r_x_min = (video_width*ratios$R_left),
+  r_y_max = (video_height*ratios$bottom),
+  r_y_min =  (video_height*ratios$top),
+  w_x_max = video_width/2 + floor((video_width*.94)/16),
+  w_x_min = video_width/2 - floor((video_width*.94)/16),
+  w_y_max = video_height/2,
+  w_y_min = video_height/2 - (video_height*2.5)/16,
+  lb_x_max = (video_width*.23),
+  lb_x_min = (video_width*.03),
+  lb_y_max = (video_height*0.92),
+  lb_y_min = (video_height*0.62),
+  rb_x_max = video_width - (video_width*.23),
+  rb_x_min = video_width - (video_width*.03),
+  rb_y_max = (video_height*0.92),
+  rb_y_min =  (video_height*0.62),
+  lbig_x_max = (video_width*7)/16,
+  lbig_x_min = 0,
+  lbig_y_max = video_height, 
+  lbig_y_min = ((video_height*9)/16),
+  rbig_x_max = video_width,
+  rbig_x_min = ((video_width*9)/16),
+  rbig_y_max = video_height,
+  rbig_y_min = ((video_height*9)/16)
+)
+
+
+function(xy_timepoints, trials, screen_width, screen_height){
+  xy_timepoints %>% 
+  left_join(trials) %>%
+  left_join(trial_types) %>%
+  mutate(x = x+(screen_width-video_width)/2,
+         y = y+(screen_height-video_height)/2,
+         aoi_timepoint_id = rownumber(),
+         side = dplyr::case_when(x >  l_x_min & x < l_x_max & y > l_y_min & y < l_y_max ~ "left",
+                            x > r_x_min & x < r_x_max & y > r_y_min & y < r_y_max ~ "right",
+                            x >  lb_x_min & x < lb_x_max & y > lb_y_min & y < lb_y_max ~ "left",
+                            x > rb_x_min & x < rb_x_max & y > rb_y_min & y < rb_y_max ~ "right",
+                            x > w_x_min & x < w_x_max & y > w_y_min & y < w_y_max ~ "window",
+                            x > lbig_x_min & x < lbig_x_max & y > lbig_y_min & y < lbig_y_max ~ "left",
+                            x > rbig_x_min & x < rbig_x_max & y > rbig_y_min & y < rbig_y_max ~ "right",
+                            !is.na(x) & !is.na(y) ~ "other",
+                            TRUE ~  as.character(NA)),
+         aoitype = dplyr::case_when(x > l_x_min & x < l_x_max & y > l_y_min & y < l_y_max ~ "exit",
+                               x > r_x_min & x < r_x_max & y > r_y_min & y < r_y_max ~ "exit",
+                               x >  lb_x_min & x < lb_x_max & y > lb_y_min & y < lb_y_max ~ "box",
+                               x > rb_x_min & x < rb_x_max & y > rb_y_min & y < rb_y_max ~ "box",
+                               x > w_x_min & x < w_x_max & y > w_y_min & y < w_y_max ~ "window",
+                               x > lbig_x_min & x < lbig_x_max & y > lbig_y_min & y < lbig_y_max ~ "general",
+                               x > rbig_x_min & x < rbig_x_max & y > rbig_y_min & y < rbig_y_max ~ "general",
+                               !is.na(x) & !is.na(y) ~ "other",
+                               TRUE ~  as.character(NA)),
+        aoi = dplyr::case_when(side %in%  c("left", "right") & side == target_side ~ "target",
+                           side %in% c("left", "right") & side != target_side ~ "distractor",
+                           TRUE ~ side),
+        aoi = paste(aoi, aoitype, sep="_")) %>%
+    select(aoi_timepoint_id,
+           trial_id,
+           aoi,
+           t_norm,
+           administration_id)
+}
+
+
+
+
+
+
 # 
 # 
 # 
@@ -112,29 +136,7 @@ ratios_of_bounding_box <- function() {
 #   # first we assign aois to a side, then to a type. if it is target or distractor but not
 #   # specifically in one of the other types, case_when below assigns it to general
 #   # so left_exit + left_box + left_general is a big AOI
-#   xy_joined = xy_joined %>% dplyr::mutate(
-#     side = dplyr::case_when(x >  l_x_min & x < l_x_max & y > l_y_min & y < l_y_max ~ "left",
-#                             x > r_x_min & x < r_x_max & y > r_y_min & y < r_y_max ~ "right",
-#                             x >  lb_x_min & x < lb_x_max & y > lb_y_min & y < lb_y_max ~ "left",
-#                             x > rb_x_min & x < rb_x_max & y > rb_y_min & y < rb_y_max ~ "right",
-#                             x > w_x_min & x < w_x_max & y > w_y_min & y < w_y_max ~ "window",   
-#                             x > lbig_x_min & x < lbig_x_max & y > lbig_y_min & y < lbig_y_max ~ "left",
-#                             x > rbig_x_min & x < rbig_x_max & y > rbig_y_min & y < rbig_y_max ~ "right",                            
-#                             !is.na(x) & !is.na(y) ~ "other", 
-#                             TRUE ~  as.character(NA)),
-#     aoitype = dplyr::case_when(x >  l_x_min & x < l_x_max & y > l_y_min & y < l_y_max ~ "exit",
-#                                x > r_x_min & x < r_x_max & y > r_y_min & y < r_y_max ~ "exit",
-#                                x >  lb_x_min & x < lb_x_max & y > lb_y_min & y < lb_y_max ~ "box",
-#                                x > rb_x_min & x < rb_x_max & y > rb_y_min & y < rb_y_max ~ "box",
-#                                x > w_x_min & x < w_x_max & y > w_y_min & y < w_y_max ~ "window", 
-#                                x > lbig_x_min & x < lbig_x_max & y > lbig_y_min & y < lbig_y_max ~ "general",
-#                                x > rbig_x_min & x < rbig_x_max & y > rbig_y_min & y < rbig_y_max ~ "general",                            
-#                                !is.na(x) & !is.na(y) ~ "other", 
-#                                TRUE ~  as.character(NA)),    
-#     aoi = dplyr::case_when(side %in%  c("left", "right") & side == target_side ~ "target", 
-#                            side %in% c("left", "right") & side != target_side ~ "distractor",
-#                            TRUE ~ side),
-#     aoi = paste(aoi, aoitype, sep="_"))
+
 #   return(xy_joined)
 # }
 # 
