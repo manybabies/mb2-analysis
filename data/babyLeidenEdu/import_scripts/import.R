45ff6feddbb5dbb393dec93dae085e0e3164c6b0
alvinwmtan <alvin.tan1996@gmail.com>
2023-10-12 16:06:31 -0700

    added DevlabAU and babyLeidenEdu

---
+++ data/babyLeidenEdu/import_scripts/import.R
@@ -0,0 +1,40 @@
+library(tidyverse)
+library(here)
+library(glue)
+
+LAB_NAME <- "babyLeidenEdu"
+DATA_DIR <- here("data", LAB_NAME)
+dir.create(here(DATA_DIR, "processed_data"))
+
+#### Adult data ####
+data_adults <- read_tsv(here(DATA_DIR, "raw_data", "babyLeidenEdu_adults_eyetrackingdata.tsv"))
+
+data_adults_cleaned <- data_adults |> 
+  select(participant_id = `Participant name`,
+         x = `Gaze point X`,
+         y = `Gaze point Y`,
+         t = `Recording timestamp`,
+         media_name = `Presented Media name`,
+         pupil_left = `Pupil diameter left`,
+         pupil_right = `Pupil diameter right`) |> 
+  mutate(lab_id = LAB_NAME)
+
+write_csv(data_adults_cleaned,
+          here(DATA_DIR, "processed_data", glue("{LAB_NAME}_adults_xy_timepoints.csv")))
+
+#### Toddler data ####
+data_toddlers <- read_tsv(here(DATA_DIR, "raw_data", "babyLeidenEdu_toddlers_eyetrackingdata.tsv"))
+
+data_toddlers_cleaned <- data_toddlers |> 
+  select(participant_id = `Recording name`,
+         x = `Gaze point X`,
+         y = `Gaze point Y`,
+         t = `Recording timestamp`,
+         media_name = `Presented Media name`,
+         pupil_left = `Pupil diameter left`,
+         pupil_right = `Pupil diameter right`) |> 
+  mutate(lab_id = LAB_NAME)
+
+write_csv(data_toddlers_cleaned,
+          here(DATA_DIR, "processed_data", glue("{LAB_NAME}_toddlers_xy_timepoints.csv")))
+
