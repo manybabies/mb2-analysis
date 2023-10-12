5e556d450c7025a2bc851ef118567ebfb1288324
alvinwmtan <alvin.tan1996@gmail.com>
2023-10-12 10:26:46 -0700

    added babylabUmassb import

---
+++ data/babylabUmassb/import_scripts/import.R
@@ -0,0 +1,40 @@
+library(tidyverse)
+library(here)
+library(glue)
+
+LAB_NAME <- "babylabUmassb"
+DATA_DIR <- here("data", LAB_NAME)
+dir.create(here(DATA_DIR, "processed_data"))
+
+#### Adult data ####
+data_adults <- read_tsv(here(DATA_DIR, "raw_data", "babylabUmassb_adults_eyetrackingdata.tsv"), quote = "'")
+
+data_adults_cleaned <- data_adults |> 
+  # filter out rows with incomplete data
+  filter(!is.na(RecordingName),
+         RecordingName != "UMB\"") |> 
+  select(participant_id = ParticipantName,
+         x = `GazePointX (ADCSpx)`,
+         y = `GazePointY (ADCSpx)`,
+         t = RecordingTimestamp,
+         media_name = MediaName,
+         pupil_left = PupilLeft,
+         pupil_right = PupilRight) |> 
+  mutate(lab_id = LAB_NAME)
+
+write_csv(data_adults_cleaned,
+          here(DATA_DIR, "processed_data", glue("{LAB_NAME}_adults_xy_timepoints.csv")))
+
+#### Toddler data ####
+data_toddlers <- read_tsv(here(DATA_DIR, "raw_data", "babylabUmassb_toddlers_eyetrackingdata.tsv"))
+
+# !!! Toddler data has all NAs for media_name
+data_toddlers_cleaned <- data_toddlers |> 
+  select(participant_id = ParticipantName,
+         x = `GazePointX (ADCSpx)`,
+         y = `GazePointY (ADCSpx)`,
+         t = RecordingTimestamp,
+         media_name = MediaName,
+         pupil_left = PupilLeft,
+         pupil_right = PupilRight) |> 
+  mutate(lab_id = LAB_NAME)
