7dba632d1cb0895c46df7d23f3015a095a1957cd
mzettersten <mzettersten@users.noreply.github.com>
2023-10-13 11:58:38 -0700

    update resampling function to include pupil data
    + pupil data resampled using constant interpolation
    + make the resampling process column agnostic, i.e. keep all (constant, non-time-varying) columns in the resampled data frame

--- helper/resampling_helper.R
+++ helper/resampling_helper.R
@@ -37,7 +37,7 @@ normalize_times <- function(df_table,trial_col_name = "event_num") {
   return(df_out)
 }
 
-resample_xy_trial <- function(df_trial, timepoint_col_name = "t_norm",trial_col_name = "event_num") {
+resample_xy_trial <- function(df_trial, timepoint_col_name = "t_norm",trial_col_name = "event_num",resample_pupil_size=TRUE) {
   MISSING_CONST <- -10000
   # set sample rates
   SAMPLE_RATE = 40 # Hz
@@ -47,6 +47,12 @@ resample_xy_trial <- function(df_trial, timepoint_col_name = "t_norm",trial_col_
   x_origin <- df_trial$x
   y_origin <- df_trial$y
   
+  #also set up for resampling pupil size
+  if (resample_pupil_size) {
+    pupil_left_origin <- df_trial$pupil_left
+    pupil_right_origin <- df_trial$pupil_right
+  }
+  
   # create the new timestamps for resampling
   t_start <- min(t_origin) - (min(t_origin) %% SAMPLE_DURATION)
   t_resampled <- seq(from = t_start, to = max(t_origin),
@@ -55,6 +61,11 @@ resample_xy_trial <- function(df_trial, timepoint_col_name = "t_norm",trial_col_
   # because of the behavior of approx, we need numerical values for missingness
   x_origin[is.na(x_origin)] <- MISSING_CONST
   y_origin[is.na(y_origin)] <- MISSING_CONST
+  
+  if (resample_pupil_size) {
+    pupil_left_origin[is.na(pupil_left_origin)] <- MISSING_CONST
+    pupil_right_origin[is.na(pupil_right_origin)] <- MISSING_CONST
+  }
   
   # resample we use constant interpolation for two reasons: 1) the numerical
   # missingness needs to be constant, if you interpolate it you won't be able to
@@ -67,32 +78,82 @@ resample_xy_trial <- function(df_trial, timepoint_col_name = "t_norm",trial_col_
                                method = "constant", rule = 2,
                                ties = "ordered")$y
   
+  if (resample_pupil_size) {
+    pupil_left_resampled <- stats::approx(x = t_origin, y = pupil_left_origin, xout = t_resampled,
+                                 method = "constant", rule = 2,
+                                 ties = "ordered")$y
+    pupil_right_resampled <- stats::approx(x = t_origin, y = pupil_right_origin, xout = t_resampled,
+                                 method = "constant", rule = 2,
+                                 ties = "ordered")$y
+  }
+  
   # replace missing values
   x_resampled[x_resampled == MISSING_CONST] <- NA
   y_resampled[y_resampled == MISSING_CONST] <- NA
   
+  if (resample_pupil_size) {
+    pupil_left_resampled[pupil_left_resampled == MISSING_CONST] <- NA
+    pupil_right_resampled[pupil_right_resampled == MISSING_CONST] <- NA
+  }
+  
   # adding back the columns to match schema
   # note, no IDs here because they won't be unique.
-  dplyr::tibble(
-    lab_id = df_trial$lab_id[1],
-    participant_id = df_trial$participant_id[1],
-    {{trial_col_name}} := df_trial[[trial_col_name]][1],
-    {{timepoint_col_name}} := t_resampled,
-    x = x_resampled,
-    y = y_resampled,
-    media_name = df_trial$media_name[1],
-    media_version = df_trial$media_version[1],
-    trial_num = df_trial$trial_num[1],
-    age_cohort = df_trial$age_cohort[1],
-    target = df_trial$target[1]
-    )
+  # dplyr::tibble(
+  #   lab_id = df_trial$lab_id[1],
+  #   participant_id = df_trial$participant_id[1],
+  #   {{trial_col_name}} := df_trial[[trial_col_name]][1],
+  #   {{timepoint_col_name}} := t_resampled,
+  #   x = x_resampled,
+  #   y = y_resampled,
+  #   media_name = df_trial$media_name[1],
+  #   media_version = df_trial$media_version[1],
+  #   trial_num = df_trial$trial_num[1],
+  #   age_cohort = df_trial$age_cohort[1],
+  #   target = df_trial$target[1]
+  #   )
+  if (resample_pupil_size) {
+    #first create a dataset with all constant columns and expand to the length of the resampled trial data frame
+    df_trial_col_data_single_row <- df_trial %>%
+      ungroup() %>%
+      select(-x,-y,-!!sym(timepoint_col_name),-!!sym(trial_col_name),-pupil_left,-pupil_right,-t,-t_zeroed) %>%
+      summarize(across(everything(), first))
+    df_trial_col_data <- df_trial_col_data_single_row[rep(1, length(t_resampled)), ]
+    #add the new resampled columns
+    df_trial_new <- df_trial_col_data %>%
+      mutate(
+        {{trial_col_name}} := df_trial[[trial_col_name]][1],
+        {{timepoint_col_name}} := t_resampled,
+        x = x_resampled,
+        y = y_resampled,
+        pupil_left = pupil_left_resampled,
+        pupil_right = pupil_right_resampled
+      )
+    
+  } else {
+    #first create a dataset with all constant columns and expand to the length of the resampled trial data frame
+    df_trial_col_data_single_row <- df_trial %>%
+      ungroup() %>%
+      select(-x,-y,-!!sym(timepoint_col_name),-!!sym(trial_col_name),-pupil_left,-pupil_right,-t,-t_zeroed) %>%
+      summarize(across(everything(), first))
+    df_trial_col_data <- df_trial_col_data_single_row[rep(1, length(t_resampled)), ]
+    #add the new resampled columns
+    df_trial_new <- df_trial_col_data %>%
+      mutate(
+        {{trial_col_name}} := df_trial[[trial_col_name]][1],
+        {{timepoint_col_name}} := t_resampled,
+        x = x_resampled,
+        y = y_resampled
+      )
+  }
+
+  df_trial_new
 }
 
 resample_times <- function(df_table,timepoint_col_name = "t_norm",trial_col_name = "event_num") {
   
   # first check if this data frame has all the correct columns required for
   # re-sampling
-  required_columns <- c(trial_col_name, "participant_id", timepoint_col_name, "x", "y")
+  required_columns <- c(trial_col_name, "participant_id", timepoint_col_name, "x", "y","pupil_left","pupil_right")
   
   # re-zero and normalize times first
   # this is mandatory, comes from our decision that not linking resampling and
