90e2e2b4c4308d1ae59bfe8b963ea5618b664ef2
alvinwmtan <alvin.tan1996@gmail.com>
2023-10-14 07:33:29 -0700

    added kokuHamburg and mpievaCCP

---
+++ import_scripts/kokuHamburg/import_scripts/import.R
@@ -0,0 +1,25 @@
+library(tidyverse)
+library(here)
+library(glue)
+library(vroom)
+
+LAB_NAME <- "kokuHamburg"
+DATA_DIR <- here("import_scripts", LAB_NAME)
+dir.create(here(DATA_DIR, "processed_data"))
+
+#### Toddler data ####
+data_toddlers <- vroom(here(DATA_DIR, "raw_data", "kokuHamburg_toddlers_eyetrackingdata.tsv"),
+                       delim = "\t")
+
+data_toddlers_cleaned <- data_toddlers |> 
+  select(participant_id = `Participant name`,
+         x = `Gaze point X [DACS px]`,
+         y = `Gaze point Y [DACS px]`,
+         t = `Recording timestamp [ms]`,
+         media_name = `Presented Media name`,
+         pupil_left = `Pupil diameter left [mm]`,
+         pupil_right = `Pupil diameter right [mm]`) |> 
+  mutate(lab_id = LAB_NAME)
+
+write_csv(data_toddlers_cleaned,
+          here(DATA_DIR, "processed_data", glue("{LAB_NAME}_toddlers_xy_timepoints.csv")))
---
+++ import_scripts/mpievaCCP/import_scripts/import.R
@@ -0,0 +1,36 @@
+library(tidyverse)
+library(here)
+library(glue)
+
+LAB_NAME <- "mpievaCCP"
+DATA_DIR <- here("import_scripts", LAB_NAME)
+dir.create(here(DATA_DIR, "processed_data"))
+
+#### Toddler data ####
+data_toddlers1 <- read_tsv(here(DATA_DIR, "raw_data", "mpievaCCP_toddlers_eyetrackingdata_TrialBin3.tsv"))
+data_toddlers2 <- read_tsv(here(DATA_DIR, "raw_data", "mpievaCCP_toddlers_eyetrackingdata_TrialBin4.tsv"))
+
+data_toddlers1_cleaned <- data_toddlers1 |> 
+  select(participant_id = `Recording name`,
+         x = `Gaze point X`,
+         y = `Gaze point Y`,
+         t = `Recording timestamp`,
+         media_name = `Presented Media name`,
+         pupil_left = `Pupil diameter left`,
+         pupil_right = `Pupil diameter right`) |> 
+  mutate(lab_id = LAB_NAME)
+data_toddlers2_cleaned <- data_toddlers2 |> 
+  select(participant_id = `Recording name`,
+         x = `Gaze point X`,
+         y = `Gaze point Y`,
+         t = `Recording timestamp`,
+         media_name = `Presented Media name`,
+         pupil_left = `Pupil diameter left`,
+         pupil_right = `Pupil diameter right`) |> 
+  mutate(lab_id = LAB_NAME)
+data_toddlers_cleaned <- bind_rows(data_toddlers1_cleaned, data_toddlers2_cleaned) |> 
+  mutate(t = t / 1000)
+
+write_csv(data_toddlers_cleaned,
+          here(DATA_DIR, "processed_data", glue("{LAB_NAME}_toddlers_xy_timepoints.csv")))
+
