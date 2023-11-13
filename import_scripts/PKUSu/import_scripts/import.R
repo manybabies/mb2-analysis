520278b86e4670dfa9f7be3192f8fc194ca14c62
Yanwei Wang <147751071+yanwei-W@users.noreply.github.com>
2023-11-13 21:57:48 +0800

    Update import.R
    new import scripts of PKUSu data

--- import_scripts/PKUSu/import_scripts/import.R
+++ import_scripts/PKUSu/import_scripts/import.R
@@ -7,34 +7,39 @@ DATA_DIR = file.path('import_scripts', LAB_NAME)
 dir.create(here(DATA_DIR),"processed_data")
 
 ###Toddler data###
-data_toddlers <- read.csv(here(DATA_DIR, "raw_data","PKUSu_toddlers_eyetrackingdata.csv"), sep="\t", fileEncoding="UTF-16LE")
+PKU_toddlers <- read.csv(here(DATA_DIR, "raw_data","PKUSu_toddlers_eyetrackingdata.csv"), sep="\t", fileEncoding="UTF-16LE")
 
-data_toddlers_clean <- data_toddlers %>% 
-  select(participant_id = Participant name,
-         x = Gaze point X,
-         y = Gaze point Y,
-         t = Recording timestamp,
-         media_name = Presented Media name,
-  ) %>% 
-  mutate(lab_id = LAB_NAME,
-         pupil_left = Pupil diameter left,
-         pupil_right = Pupil diameter right)
-
-write.csv(data_toddlers_clean, here(DATA_DIR,"processed_data", glue("{LAB_NAME}_toddlers_xy_timepoints.csv")))
-
-###Adult data###
-data_adults <- read.csv(here(DATA_DIR, "raw_data","PKUSu_adults_eyetrackingdata.csv"),sep="\t", fileEncoding="UTF-16LE")
-
-data_adults_clean <- data_adults %>% 
-  select(participant_id = Participant name,
-         x = Gaze point X,
-         y = Gaze point Y,
-         t = Recording timestamp,
-         media_name = Presented Media name,
+PKU_toddlers[PKU_toddlers==""]<-NA
+PKU_toddlers_clean <- PKU_toddlers %>% 
+  rename(participant_id = Participant.name,
+         x = Gaze.point.X,
+         y = Gaze.point.Y,
+         t = Recording.timestamp,
+         media_name = Presented.Media.name,
+         pupil_left = Pupil.diameter.left,
+         pupil_right = Pupil.diameter.right
   ) %>% 
   mutate(media_name=str_replace_all(media_name,"_new",""),
-         lab_id = LAB_NAME,
-         pupil_left = Pupil diameter left,
-         pupil_right = Pupil diameter right)
+         lab_id = LAB_NAME)%>%
+  select(lab_id,participant_id,media_name,x,y,t,pupil_left,pupil_right)
 
-write.csv(data_adults_clean, here(DATA_DIR,"processed_data", glue("{LAB_NAME}_adults_xy_timepoints.csv")))
+write_csv(PKU_toddlers_clean, here(DATA_DIR,"processed_data", glue("{LAB_NAME}_toddlers_xy_timepoints.csv")))
+
+###Adult data###
+PKU_adults <- read.csv(here(DATA_DIR, "raw_data","PKUSu_adults_eyetrackingdata.csv"),sep="\t", fileEncoding="UTF-16LE")
+
+PKU_adults[PKU_adults==""]<-NA
+PKU_adults_clean <- PKU_adults %>% 
+  rename(participant_id = Participant.name,
+         x = Gaze.point.X,
+         y = Gaze.point.Y,
+         t = Recording.timestamp,
+         media_name = Presented.Media.name,
+         pupil_left = Pupil.diameter.left,
+         pupil_right = Pupil.diameter.right
+  )%>% 
+  mutate(media_name=str_replace_all(media_name,"_new",""),
+         lab_id = LAB_NAME)%>%
+  select(lab_id,participant_id,media_name,x,y,t,pupil_left,pupil_right)
+
+write_csv(PKU_adults_clean, here(DATA_DIR,"processed_data", glue("{LAB_NAME}_adults_xy_timepoints.csv")))
