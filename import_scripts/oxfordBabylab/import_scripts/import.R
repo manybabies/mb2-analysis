6985fcdcefb36a2028a8eb9bbb3cbd472b4597a3
Yanwei Wang <147751071+yanwei-W@users.noreply.github.com>
2023-11-13 22:02:31 +0800

    Create oxf import.R
    oxfordBabylab

---
+++ import_scripts/oxfordBabylab/import_scripts/import.R
@@ -0,0 +1,25 @@
+library(tidyverse)
+library(here)
+library(glue)
+
+LAB_NAME <- 'oxfordBabylab'
+DATA_DIR = file.path('import_scripts', LAB_NAME)
+dir.create(here(DATA_DIR),"processed_data")
+
+###Toddler data###
+oxf_toddlers <- read_tsv('oxfordBabylab_toddlers_eyetrackingdata.tsv')
+
+oxf_toddlers_clean <- oxf_toddlers %>% 
+  rename(participant_id = `Participant name`,
+         x = `Gaze point X`,
+         y = `Gaze point Y`,
+         t = `Recording timestamp`,
+         media_name = `Presented Media name`,
+         pupil_left = `Pupil diameter left`,
+         pupil_right = `Pupil diameter right`
+  ) %>% 
+  mutate(lab_id = LAB_NAME,
+         media_name=str_replace_all(media_name,"_new",""))%>%
+  select(lab_id,participant_id,media_name,x,y,t,pupil_left,pupil_right)
+
+write_csv(oxf_toddlers_clean, here(DATA_DIR,"processed_data", glue("{LAB_NAME}_toddlers_xy_timepoints.csv")))
