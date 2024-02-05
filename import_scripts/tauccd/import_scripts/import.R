00a536c71dbaeb1a2101927227e2bc9e88de6dea
alvinwmtan <alvin.tan1996@gmail.com>
2024-02-05 15:53:01 -0800

    added tauccd participant_id

--- import_scripts/tauccd/import_scripts/import.R
+++ import_scripts/tauccd/import_scripts/import.R
@@ -3,6 +3,8 @@ library(here)
 library(glue)
 library(eyelinkReader)
 
+# NOTE: Requires demog data to get true participant IDs
+
 LAB_NAME <- "tauccd"
 DATA_DIR <- here("import_scripts", LAB_NAME)
 dir.create(here(DATA_DIR, "processed_data"))
@@ -11,6 +13,7 @@ dir.create(here(DATA_DIR, "processed_data"))
 data_path <- here(DATA_DIR, "raw_data", "EDF files ")
 data_files <- list.files(data_path, pattern = ".edf", recursive = TRUE)
 data_files_adults <- data_files[17:32]
+data_demog_adults <- read_csv(here(DATA_DIR, "raw_data", "tauccd_adults_participantdata.csv"))
 
 data_adults_cleaned <- lapply(data_files_adults, \(fp) {
   data_edf <- read_edf(here(data_path, fp),
@@ -40,11 +43,20 @@ data_adults_cleaned <- lapply(data_files_adults, \(fp) {
   samples
 }) |> bind_rows()
 
+data_adults_cleaned <- data_adults_cleaned |> 
+  left_join(data_demog_adults |> 
+              select(participant_id, test_order) |> 
+              mutate(test_order = as.character(test_order)), 
+            by = c("participant_id" = "test_order")) |> 
+  mutate(participant_id = participant_id.y) |> 
+  select(-participant_id.y)
+
 write_csv(data_adults_cleaned,
           here(DATA_DIR, "processed_data", glue("{LAB_NAME}_adults_xy_timepoints.csv")))
 
 #### Toddler data ####
 data_files_toddlers <- data_files[1:16]
+data_demog_toddlers <- read_csv(here(DATA_DIR, "raw_data", "tauccd_toddlers_participantdata.csv"))
 
 data_toddlers_cleaned <- lapply(data_files_toddlers, \(fp) {
   data_edf <- read_edf(here(data_path, fp),
@@ -84,6 +96,14 @@ data_toddlers_cleaned <- lapply(data_files_toddlers, \(fp) {
   
   samples
 }) |> bind_rows()
+
+data_toddlers_cleaned <- data_toddlers_cleaned |> 
+  left_join(data_demog_toddlers |> 
+              select(participant_id, test_order) |> 
+              mutate(test_order = as.character(test_order)), 
+            by = c("participant_id" = "test_order")) |> 
+  mutate(participant_id = participant_id.y) |> 
+  select(-participant_id.y)
 
 write_csv(data_toddlers_cleaned,
           here(DATA_DIR, "processed_data", glue("{LAB_NAME}_toddlers_xy_timepoints.csv")))
