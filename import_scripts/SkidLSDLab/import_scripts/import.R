361211ea8a7f3ff90ae84a40364e29c919533818
Melanie S Schreiner <melanie.schreiner@psych.uni-goettingen.de>
2024-04-11 21:12:31 +0200

    updated import script for SkidLSDLab
    replacing 0 as NAs

--- import_scripts/SkidLSDLab/import_scripts/import.R
+++ import_scripts/SkidLSDLab/import_scripts/import.R
@@ -7,9 +7,13 @@ DATA_DIR = file.path('import_scripts', LAB_NAME)
 dir.create(here(DATA_DIR),"processed_data")
 
 ###Toddler data###
-Skid_toddlers <- read_csv(here(DATA_DIR,"raw_data","SkidLSDLab_toddlers_eyetrackingdata.csv"))
+Skid_toddlers <- read_excel(here(DATA_DIR,"raw_data","SkidLSDLab_toddlers_eyetrackingdata.xlsx"))
 
 Skid_toddlers[Skid_toddlers=="-"]<-NA
+Skid_toddlers$`Point of Regard Right X [px]`[Skid_toddlers$`Point of Regard Right X [px]`==0]<-NA
+Skid_toddlers$`Point of Regard Left X [px]`[Skid_toddlers$`Point of Regard Left X [px]`==0]<-NA
+Skid_toddlers$`Point of Regard Right Y [px]`[Skid_toddlers$`Point of Regard Right Y [px]`==0]<-NA
+Skid_toddlers$`Point of Regard Left Y [px]`[Skid_toddlers$`Point of Regard Left Y [px]`==0]<-NA
 Skid_toddlers$`Point of Regard Right X [px]`<-as.numeric(Skid_toddlers$`Point of Regard Right X [px]`)
 Skid_toddlers$`Point of Regard Left X [px]`<-as.numeric(Skid_toddlers$`Point of Regard Left X [px]`)
 Skid_toddlers$`Point of Regard Right Y [px]`<-as.numeric(Skid_toddlers$`Point of Regard Right Y [px]`)
@@ -17,6 +21,9 @@ Skid_toddlers$`Point of Regard Left Y [px]`<-as.numeric(Skid_toddlers$`Point of 
 
 Skid_toddlers<-transform(Skid_toddlers,x = (`Point of Regard Right X [px]`+`Point of Regard Left X [px]`)/2,
                          y = (`Point of Regard Right Y [px]`+`Point of Regard Left Y [px]`)/2)
+
+Skid_toddlers$`Pupil.Diameter.Left..mm.`[Skid_toddlers$`Pupil.Diameter.Left..mm.`==0]<-NA
+Skid_toddlers$`Pupil.Diameter.Right..mm.`[Skid_toddlers$`Pupil.Diameter.Right..mm.`==0]<-NA
 
 Skid_toddlers_clean <- Skid_toddlers %>% 
   rename(participant_id = Participant,
@@ -32,9 +39,13 @@ Skid_toddlers_clean <- Skid_toddlers %>%
 write_csv(Skid_toddlers_clean, here(DATA_DIR,"processed_data", glue("{LAB_NAME}_toddlers_xy_timepoints.csv")))
 
 ###Adult data###
-Skid_adults <- read_csv(here(DATA_DIR,"raw_data","SkidLSDLab_adults_eyetrackingdata.tsv"))
+Skid_adults <- read_excel(here(DATA_DIR,"raw_data","SkidLSDLab_adults_eyetrackingdata.xlsx"))
 
 Skid_adults[Skid_adults=="-"]<-NA
+Skid_adults$`Point of Regard Right X [px]`[Skid_adults$`Point of Regard Right X [px]`==0]<-NA
+Skid_adults$`Point of Regard Left X [px]`[Skid_adults$`Point of Regard Left X [px]`==0]<-NA
+Skid_adults$`Point of Regard Right Y [px]`[Skid_adults$`Point of Regard Right Y [px]`==0]<-NA
+Skid_adults$`Point of Regard Left Y [px]`[Skid_adults$`Point of Regard Left Y [px]`==0]<-NA
 Skid_adults$`Point of Regard Right X [px]`<-as.numeric(Skid_adults$`Point of Regard Right X [px]`)
 Skid_adults$`Point of Regard Left X [px]`<-as.numeric(Skid_adults$`Point of Regard Left X [px]`)
 Skid_adults$`Point of Regard Right Y [px]`<-as.numeric(Skid_adults$`Point of Regard Right Y [px]`)
@@ -42,6 +53,9 @@ Skid_adults$`Point of Regard Left Y [px]`<-as.numeric(Skid_adults$`Point of Rega
 
 Skid_adults<-transform(Skid_adults,x = (`Point of Regard Right X [px]`+`Point of Regard Left X [px]`)/2,
                        y = (`Point of Regard Right Y [px]`+`Point of Regard Left Y [px]`)/2)
+
+Skid_adults$`Pupil.Diameter.Left..mm.`[Skid_adults$`Pupil.Diameter.Left..mm.`==0]<-NA
+Skid_adults$`Pupil.Diameter.Right..mm.`[Skid_adults$`Pupil.Diameter.Right..mm.`==0]<-NA
 
 Skid_adults_clean <- Skid_adults %>% 
   rename(participant_id = Participant,
