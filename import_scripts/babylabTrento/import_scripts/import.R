6c6fbb28e3cef45d6593e2dbf2bf499ca74fa042
Melanie S Schreiner <melanie.schreiner@psych.uni-goettingen.de>
2024-05-28 22:55:01 +0200

    Fix pupil size import for babylabTrento

--- import_scripts/babylabTrento/import_scripts/import.R
+++ import_scripts/babylabTrento/import_scripts/import.R
@@ -7,7 +7,7 @@ DATA_DIR <- here("import_scripts", LAB_NAME)
 dir.create(here(DATA_DIR, "processed_data"))
 
 #### Adult data ####
-data_adults <- read_tsv(here(DATA_DIR, "raw_data", "babylabTrento_adults_eyetrackingdata.tsv"))
+data_adults <- read_delim(here(DATA_DIR, "raw_data/babylabTrento_adults_eyetrackingdata.tsv"), delim = "\t", escape_double = FALSE, locale = locale(decimal_mark = ",", grouping_mark = ""), trim_ws = TRUE)
 
 data_adults_cleaned <- data_adults |>
   select(participant_id = `Participant name`,
@@ -24,7 +24,7 @@ write_csv(data_adults_cleaned,
           here(DATA_DIR, "processed_data", glue("{LAB_NAME}_adults_xy_timepoints.csv")))
 
 #### Toddler data ####
-data_toddlers <- read_tsv(here(DATA_DIR, "raw_data", "babylabTrento_toddlers_eyetrackingdata.tsv"))
+data_toddlers <- data_adults <- read_delim(here(DATA_DIR, "raw_data/babylabTrento_toddlers_eyetrackingdata.tsv"), delim = "\t", escape_double = FALSE, locale = locale(decimal_mark = ",", grouping_mark = ""), trim_ws = TRUE)
 
 data_toddlers_cleaned <- data_toddlers |> 
   select(participant_id = `Participant name`,
