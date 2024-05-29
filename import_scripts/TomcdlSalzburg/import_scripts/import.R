c1842b2e4a959c2f0e9e5665348b1ba98fbd9983
Melanie S Schreiner <melanie.schreiner@psych.uni-goettingen.de>
2024-05-29 14:53:37 +0200

    Fix pupil size import for TomcdlSalzburg

--- import_scripts/TomcdlSalzburg/import_scripts/import.R
+++ import_scripts/TomcdlSalzburg/import_scripts/import.R
@@ -8,12 +8,17 @@ lab_dataset_name <- "ToMcdlSalzburg_adults"
 
 #dataset 1
 # eye-tracking data
-dataset1 <- read_tsv(here(lab_dir, "raw_data","ToMcdlSalzburg_adults_eyetrackingdata_bin1.tsv" ) )
+dataset1 <- read_delim(here(lab_dir, "raw_data/ToMcdlSalzburg_adults_eyetrackingdata_bin1.tsv"), delim = "\t", escape_double = FALSE, locale = locale(decimal_mark = ",", grouping_mark = ""), trim_ws = TRUE)
+
+  read_tsv(here(lab_dir, "raw_data","ToMcdlSalzburg_adults_eyetrackingdata_bin1.tsv" ) )
 dataset1 <- dataset1 %>%
   janitor::clean_names()
 
 # following data import guide:
 # https://docs.google.com/document/d/1MEEQicPc1baABDHFasbWoujvj2GwfBGarwrzyS2JQtM/edit
+
+dataset1$'pupil_left'[dataset1$'pupil_left'==-1]<-NA
+dataset1$'pupil_right'[dataset1$'pupil_right'==-1]<-NA
 
 # xy_timepoints
 xy_timepoints_1 <- dataset1 |>
@@ -27,12 +32,16 @@ xy_timepoints_1 <- dataset1 |>
 
 #dataset2
 # eye-tracking data
-dataset2 <- read_tsv(here(lab_dir, "raw_data","ToMcdlSalzburg_adults_eyetrackingdata_bin2.tsv" ) )
+dataset2 <- read_delim(here(lab_dir, "raw_data/ToMcdlSalzburg_adults_eyetrackingdata_bin2.tsv"), delim = "\t", escape_double = FALSE, locale = locale(decimal_mark = ",", grouping_mark = ""), trim_ws = TRUE)
+  read_tsv(here(lab_dir, "raw_data","ToMcdlSalzburg_adults_eyetrackingdata_bin2.tsv" ) )
 dataset2 <- dataset2 %>%
   janitor::clean_names()
 
 # following data import guide:
 # https://docs.google.com/document/d/1MEEQicPc1baABDHFasbWoujvj2GwfBGarwrzyS2JQtM/edit
+
+dataset2$'pupil_left'[dataset2$'pupil_left'==-1]<-NA
+dataset2$'pupil_right'[dataset2$'pupil_right'==-1]<-NA
 
 # xy_timepoints
 xy_timepoints_2 <- dataset2 |>
@@ -51,12 +60,19 @@ write_csv(xy_timepoints, here(lab_dir, "processed_data",paste0(lab_dataset_name,
 #toddlers
 lab_dataset_name_toddlers <- "ToMcdlSalzburg_toddlers"
 # eye-tracking data
-d_toddlers <- read_tsv(here(lab_dir, "raw_data","ToMcdlSalzburg_toddlers_eyetrackingdata.tsv" ) )
+d_toddlers <- read_delim(here(lab_dir, "raw_data/ToMcdlSalzburg_toddlers_eyetrackingdata.tsv"), delim = "\t", escape_double = FALSE, locale = locale(decimal_mark = ",", grouping_mark = ""), trim_ws = TRUE)
+
+  read_tsv(here(lab_dir, "raw_data","ToMcdlSalzburg_toddlers_eyetrackingdata.tsv" ) )
 d_toddlers <- d_toddlers %>%
   janitor::clean_names()
 
 # following data import guide:
 # https://docs.google.com/document/d/1MEEQicPc1baABDHFasbWoujvj2GwfBGarwrzyS2JQtM/edit
+d_toddlers$'pupil_left' <- as.numeric(d_toddlers$'pupil_left')
+d_toddlers$'pupil_right' <- as.numeric(d_toddlers$'pupil_right')
+
+d_toddlers$'pupil_left'[d_toddlers$'pupil_left'==-1]<-NA
+d_toddlers$'pupil_right'[d_toddlers$'pupil_right'==-1]<-NA
 
 # xy_timepoints
 xy_timepoints_toddlers <- d_toddlers |>
